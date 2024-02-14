c80e4bbf4fde66a8215962a0069a91f34001a245
Antoine <me@antoinebellanger.fr>
2024-02-14 17:03:00 +0100

    [ENHANCEMENT] What a beautiful way of triggering a segfault

--- bonus/segfault.c
+++ bonus/segfault.c
@@ -5,13 +5,4 @@
 ** segfault header
 */
 
-#include <stddef.h>
-#include <stdio.h>
-
-void main(void)
-{
-    printf("Going to segfault in 3.. 2.. 1..\n");
-    int *ptr = NULL;
-
-    *ptr = 42;
-}
+main;
