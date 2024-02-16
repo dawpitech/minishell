0bfe4ad1dfee8b3ff67ccfe96c43a9d7bc6bcadf
Antoine <me@antoinebellanger.fr>
2024-02-16 11:45:30 +0100

    [ENHANCEMENT] CD builtin now goes home if no arguments are given Also send an error if too many arguments are given.

--- sources/builtins/builtins_cmd.c
+++ sources/builtins/builtins_cmd.c
@@ -16,18 +16,22 @@
 #include "my_printf.h"
 
 static
-char *compute_cd_path(shell_t *shell, char **argv)
+char *compute_cd_path(shell_t *shell, int argc, char **argv)
 {
     env_var_t *home_var = get_env_var(shell, "HOME");
 
-    if (my_strcmp(argv[1], "-") == 0)
-        return shell->last_path == NULL ? "\0" : my_strdup(shell->last_path);
-    if (my_strcmp(argv[1], "~") == 0) {
+    if (argc > 2) {
+        my_put_stderr("cd: Too many arguments.");
+        return NULL;
+    }
+    if (argc == 1 || my_strcmp(argv[1], "~") == 0) {
         if (home_var != NULL)
             return my_strdup(home_var->value);
         my_put_stderr("No $home variable set.\n");
         return NULL;
     }
+    if (my_strcmp(argv[1], "-") == 0)
+        return shell->last_path == NULL ? "\0" : my_strdup(shell->last_path);
     return my_strdup(argv[1]);
 }
 
@@ -58,7 +62,7 @@ void update_shell_ref(shell_t *shell, char *new_path, char *old_path)
 int execute_cd(shell_t *shell, __attribute__((unused)) int argc, char **argv)
 {
     int rt_val;
-    char *new_path = compute_cd_path(shell, argv);
+    char *new_path = compute_cd_path(shell, argc, argv);
     char *old_path = getcwd(NULL, 0);
 
     if (new_path == NULL)
